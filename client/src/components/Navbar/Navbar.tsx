e4b6dfe960193d79a4eb7b6b5d48e211e89a08d4
andbudha <andreibartov@gmail.com>
2024-07-21 16:10:03 +0200

    Navbar comp updated/active link styles&functionality added

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,37 +1,58 @@
 import { useState } from 'react';
 import styles from './Navbar.module.scss';
 import { TbEdit } from 'react-icons/tb';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 export const Navbar = () => {
-  const [user, setUser] = useState<boolean>(false);
+  const [user, setUser] = useState<boolean>(true);
+  const path = useLocation();
+  console.log(path);
+
   return (
     <div className={styles.main_navbar_box}>
-      <div className={styles.main_logo_box}>
+      <Link className={styles.main_logo_box} to={'/blogs'}>
         <TbEdit className={styles.pen_icon} />
         <h3 className={styles.logo}>blog it</h3>
-      </div>
+      </Link>
       <div className={styles.main_navbar_link_box}>
         {user ? (
           <div className={styles.active_user_link_box}>
-            <div className={styles.link_box}>
+            <NavLink
+              className={`${styles.link_box} ${
+                path.pathname === '/myblogs' && styles.active_link
+              }`}
+              to={'/myblogs'}
+            >
               <h3 className={styles.link_text}>my-blogs</h3>
-            </div>
-            <div className={styles.link_box}>
+            </NavLink>
+            <NavLink
+              className={`${styles.link_box} ${
+                path.pathname === '/myfavorites' && styles.active_link
+              }`}
+              to={'/myfavorites'}
+            >
               <h3 className={styles.link_text}>my-favorites</h3>
-            </div>
+            </NavLink>
 
-            <div className={styles.link_box}>
+            <NavLink
+              className={`${styles.link_box} ${
+                path.pathname === '/profile' && styles.active_link
+              }`}
+              to={'/profile'}
+            >
               <h3 className={styles.link_text}>profile</h3>
-            </div>
+            </NavLink>
             <div className={styles.link_box}>
-              <h3 className={styles.link_text}>logout</h3>
+              <h3 className={`${styles.link_text} ${styles.logout_button}`}>
+                logout
+              </h3>
             </div>
           </div>
         ) : (
           <div className={styles.inactive_user_link_box}>
-            <div className={styles.link_box}>
+            <NavLink className={styles.link_box} to={'signup'}>
               <h3 className={styles.link_text}>signup</h3>
-            </div>
+            </NavLink>
           </div>
         )}
       </div>
