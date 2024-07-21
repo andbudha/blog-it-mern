09728ac2960ca701c0667b521a594a301e29a572
andbudha <andreibartov@gmail.com>
2024-07-21 16:53:12 +0200

    PageNotFound created&styled

---
+++ client/src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,8 @@
+import styles from './PageNotFound.module.scss';
+export const PageNotFound = () => {
+  return (
+    <div className={styles.main_page_not_found_box}>
+      <h1 className={styles.main_text}>404 | Page Not Found</h1>
+    </div>
+  );
+};
