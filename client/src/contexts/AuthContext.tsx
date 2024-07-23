b7357bebcb515d5fa23e0484b34124932c03c59b
andbudha <andreibartov@gmail.com>
2024-07-23 15:20:40 +0200

    signup-state-variables moved into AuthContext

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,11 +1,50 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 
-type AuthContextType = {};
-const initialAuthContextState = {} as AuthContextType;
+type AuthContextType = {
+  signupEmailValue: string;
+  signupFirstNameValue: string;
+  signupSecondNameValue: string;
+  signupPasswordValue: string;
+  setSignupEmailValue: (newValue: string) => void;
+  setSignupFirstNameValue: (newValue: string) => void;
+  setSignupSecondNameValue: (newValue: string) => void;
+  setSignupPasswordValue: (newValue: string) => void;
+};
+const initialAuthContextState = {
+  signupEmailValue: '',
+  signupFirstNameValue: '',
+  signupSecondNameValue: '',
+  signupPasswordValue: '',
+  setSignupEmailValue: (newValue: string) => newValue,
+  setSignupFirstNameValue: (newValue: string) => newValue,
+  setSignupSecondNameValue: (newValue: string) => newValue,
+  setSignupPasswordValue: (newValue: string) => newValue,
+} as AuthContextType;
 export const AuthContext = createContext(initialAuthContextState);
 
 type AuthProviderProps = { children: ReactNode };
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [signupEmailValue, setSignupEmailValue] = useState<string>('');
+  const [signupFirstNameValue, setSignupFirstNameValue] = useState<string>('');
+  const [signupSecondNameValue, setSignupSecondNameValue] =
+    useState<string>('');
+  const [signupPasswordValue, setSignupPasswordValue] = useState<string>('');
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signupEmailValue,
+        signupFirstNameValue,
+        signupSecondNameValue,
+        signupPasswordValue,
+        setSignupEmailValue,
+        setSignupFirstNameValue,
+        setSignupSecondNameValue,
+        setSignupPasswordValue,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
