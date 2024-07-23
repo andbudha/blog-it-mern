600b8df8aeaf7642725c0e85a4731cfce59b1478
andbudha <andreibartov@gmail.com>
2024-07-23 16:27:53 +0200

    userSchema updated

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  email: { type: String, required: true },
   firstName: { type: String, required: true },
   secondName: { type: String, required: true },
+  password: { type: String, required: true },
+  profileImage: { type: String },
+  blogList: [],
+  favoriteBlogList: [],
 });
 export const UserModel = mongoose.model('user', userSchema);
