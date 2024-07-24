0d550990d1eca5c4737e2ba37f68d25fcc8e671b
andbudha <andreibartov@gmail.com>
2024-07-24 15:46:42 +0200

    registering new user on client side implemented

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,36 +1,69 @@
 import { createContext, ReactNode, useState } from 'react';
+import { CommonSignupValues, MainLoaderStatus } from '../types/common_types';
+import { successfulToast } from '../assets/toasts/successfulToast';
+import axios, { AxiosError } from 'axios';
+import { failureToast } from '../assets/toasts/failureToast';
 
 type AuthContextType = {
   signupEmailValue: string;
   signupFirstNameValue: string;
   signupSecondNameValue: string;
   signupPasswordValue: string;
+  mainLoaderStatus: string;
   setSignupEmailValue: (newValue: string) => void;
   setSignupFirstNameValue: (newValue: string) => void;
   setSignupSecondNameValue: (newValue: string) => void;
   setSignupPasswordValue: (newValue: string) => void;
+  registerUser: (signupValues: CommonSignupValues) => Promise<void>;
 };
 const initialAuthContextState = {
   signupEmailValue: '',
   signupFirstNameValue: '',
   signupSecondNameValue: '',
   signupPasswordValue: '',
+  mainLoaderStatus: 'idle',
   setSignupEmailValue: (newValue: string) => newValue,
   setSignupFirstNameValue: (newValue: string) => newValue,
   setSignupSecondNameValue: (newValue: string) => newValue,
   setSignupPasswordValue: (newValue: string) => newValue,
+  registerUser: () => Promise.resolve(),
 } as AuthContextType;
 export const AuthContext = createContext(initialAuthContextState);
 
 type AuthProviderProps = { children: ReactNode };
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [mainLoaderStatus, setMainLoaderStatus] =
+    useState<MainLoaderStatus>('idle');
   const [signupEmailValue, setSignupEmailValue] = useState<string>('');
   const [signupFirstNameValue, setSignupFirstNameValue] = useState<string>('');
   const [signupSecondNameValue, setSignupSecondNameValue] =
     useState<string>('');
   const [signupPasswordValue, setSignupPasswordValue] = useState<string>('');
 
+  const registerUser = async (signupValues: CommonSignupValues) => {
+    setMainLoaderStatus('registering');
+    try {
+      const response = await axios.post(
+        'http://localhost:5000/blogit/users/register',
+        signupValues
+      );
+      if (response) {
+        setMainLoaderStatus('idle');
+        successfulToast(response.data.message);
+        setSignupEmailValue('');
+        setSignupFirstNameValue('');
+        setSignupSecondNameValue('');
+        setSignupPasswordValue('');
+      }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof AxiosError)
+        failureToast(`${error.response?.data.errorMessage}`);
+    } finally {
+      setMainLoaderStatus('idle');
+    }
+  };
   return (
     <AuthContext.Provider
       value={{
@@ -38,10 +71,12 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
         signupFirstNameValue,
         signupSecondNameValue,
         signupPasswordValue,
+        mainLoaderStatus,
         setSignupEmailValue,
         setSignupFirstNameValue,
         setSignupSecondNameValue,
         setSignupPasswordValue,
+        registerUser,
       }}
     >
       {children}
