9957e953fae96de26157b88c17420f98eeeb10c0
andbudha <andreibartov@gmail.com>
2024-07-28 12:45:11 +0200

    Profile comp updated&restyled

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,47 +1,99 @@
-import { useContext, useState } from 'react';
+import { ChangeEvent, useContext, useRef } from 'react';
 import { Navigate } from 'react-router';
 import { AuthContext } from '../../contexts/AuthContext';
 import styles from './Profile.module.scss';
 import { PiUserLight } from 'react-icons/pi';
+import { ProfileEditForm } from '../../components/Forms/ProfileEditForm/ProfileEditForm';
+import { DataContext } from '../../contexts/DataContext';
 
 export const Profile = () => {
   const { user } = useContext(AuthContext);
-  const [activeEditForm, setActiveEditForm] = useState<boolean>(false);
+  const { activeEditForm, setActiveEditForm } = useContext(DataContext);
 
+  const selectedProfileImage = useRef<File | null>(null);
+  console.log(selectedProfileImage);
+
+  const activateEditProfileFormHandler = () => {
+    setActiveEditForm(true);
+  };
+
+  const changeProfileImageHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.files);
+    if (e.currentTarget.files && e.currentTarget.files.length > 0) {
+      selectedProfileImage.current = e.currentTarget.files[0];
+      console.log('Profile image is being updated');
+    }
+  };
   if (!user) {
     return <Navigate to={'/'} />;
   }
   return (
     <div className={styles.main_profie_box}>
       <div className={styles.profile_box}>
-        <div className={styles.profile_image_box}>
-          <PiUserLight className={styles.user_icon} />
+        <div className={styles.main_profile_image_box}>
+          <div className={styles.profile_image_box}>
+            {user.profileImage ? (
+              <img
+                src={user.profileImage}
+                alt="profile-image"
+                className={styles.profile_image}
+              />
+            ) : (
+              <PiUserLight className={styles.user_icon} />
+            )}
+          </div>
+          <div className={styles.upload_image_input_box}>
+            <label
+              htmlFor="image-input"
+              className={styles.profile_image_button}
+            >
+              change image
+            </label>
+            <input
+              type="file"
+              id="image-input"
+              className={styles.profile_image_input}
+              title="chnage image"
+              onChange={changeProfileImageHandler}
+            />
+          </div>
         </div>
-        <div className={styles.profile_details_box}>
-          <h3 className={styles.detail_header}>
-            First-name:{' '}
-            <span className={styles.detail_span}>{user.firstName}</span>
-          </h3>
-          <h3 className={styles.detail_header}>
-            Last-name:
-            <span className={styles.detail_span}>{user.lastName}</span>
-          </h3>
-          <h3 className={styles.detail_header}>
-            Age:
-            <span className={styles.detail_span}>
-              {user.age ? user.age : 'uknown'}
-            </span>
-          </h3>
-          <h3 className={styles.detail_header}>
-            Marital-status:
-            <span className={styles.detail_span}>
-              {user.maritalStatus ? user.maritalStatus : 'uknown'}
-            </span>
-          </h3>
-        </div>
-      </div>
-      <div className={styles.edit_profile_button_box}>
-        <div className={styles.edit_profile_button}>edit</div>
+        {activeEditForm ? (
+          <ProfileEditForm />
+        ) : (
+          <div className={styles.main_profile_details_box}>
+            <div className={styles.profile_details_box}>
+              <h3 className={styles.detail_header}>
+                First-name:{' '}
+                <span className={styles.detail_span}>{user.firstName}</span>
+              </h3>
+              <h3 className={styles.detail_header}>
+                Last-name:
+                <span className={styles.detail_span}>{user.lastName}</span>
+              </h3>
+              <h3 className={styles.detail_header}>
+                Age:
+                <span className={styles.detail_span}>
+                  {user.age ? user.age : 'confidential'}
+                </span>
+              </h3>
+              <h3 className={styles.detail_header}>
+                Marital-status:
+                <span className={styles.detail_span}>
+                  {user.maritalStatus ? user.maritalStatus : 'confidential'}
+                </span>
+              </h3>
+            </div>
+            <div className={styles.edit_profile_button_box}>
+              <div
+                className={styles.edit_profile_button}
+                onClick={activateEditProfileFormHandler}
+              >
+                edit profile
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
