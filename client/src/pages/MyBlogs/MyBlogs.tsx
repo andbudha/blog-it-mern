4fbeae561c0a3d8a4e3a52edf3aa184279bb56b2
andbudha <andreibartov@gmail.com>
2024-08-06 13:46:39 +0200

    MyBlogs comp upadate/info box&addblog btn added

--- client/src/pages/MyBlogs/MyBlogs.tsx
+++ client/src/pages/MyBlogs/MyBlogs.tsx
@@ -1,9 +1,30 @@
+import { useContext } from 'react';
 import styles from './MyBlogs.module.scss';
+import { DataContext } from '../../contexts/DataContext';
+import { BlogForm } from '../../components/Forms/BlogForm/BlogForm';
 
 export const MyBlogs = () => {
+  const { addBlogFormStatus, setAddBlogFormStatus } = useContext(DataContext);
+
+  const changeBlogFormStatusHandler = () => {
+    setAddBlogFormStatus(true);
+  };
   return (
-    <div className={styles.main_myblogs_box}>
-      <h1>My Blogs</h1>
+    <div className={styles.main_my_blogs_box}>
+      {addBlogFormStatus ? (
+        <BlogForm />
+      ) : (
+        <div className={styles.my_blogs_info_box}>
+          <h2>No Blogs Added.</h2>
+          <h2>Feel Free To Add One.</h2>
+          <div
+            className={styles.add_blog_button_box}
+            onClick={changeBlogFormStatusHandler}
+          >
+            <h4>add blog</h4>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
