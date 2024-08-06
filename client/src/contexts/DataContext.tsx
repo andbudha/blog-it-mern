1c45f5902914b7b57fde1b25be0216f4edf9b59b
andbudha <andreibartov@gmail.com>
2024-08-06 13:47:17 +0200

    blog form state variables&setters added

--- client/src/contexts/DataContext.tsx
+++ client/src/contexts/DataContext.tsx
@@ -3,14 +3,30 @@ import { createContext, ReactNode, useState } from 'react';
 type DataContextType = {
   dataLoaderStatus: boolean;
   customSelectStatus: boolean;
+  addBlogFormStatus: boolean;
+  addBlogTitleInputValue: string;
+  addBlogKeyWordInputValue: string;
+  addBlogContentInputValue: string;
   setDataLoaderStatus: (newStatus: boolean) => void;
   setCustomSelectStatus: (newStatus: boolean) => void;
+  setAddBlogFormStatus: (newStatus: boolean) => void;
+  setAddBlogTitleInputValue: (newValue: string) => void;
+  setAddBlogKeyWordInputValue: (newValue: string) => void;
+  setAddBlogContentInputValue: (newValue: string) => void;
 };
 const initialDataContextState = {
   dataLoaderStatus: false,
   customSelectStatus: false,
+  addBlogFormStatus: false,
+  addBlogTitleInputValue: '',
+  addBlogKeyWordInputValue: '',
+  addBlogContentInputValue: '',
   setDataLoaderStatus: (newStatus: boolean) => newStatus,
   setCustomSelectStatus: (newStatus: boolean) => newStatus,
+  setAddBlogFormStatus: (newStatus: boolean) => newStatus,
+  setAddBlogTitleInputValue: (newValue: string) => newValue,
+  setAddBlogKeyWordInputValue: (newValue: string) => newValue,
+  setAddBlogContentInputValue: (newValue: string) => newValue,
 } as DataContextType;
 export const DataContext = createContext(initialDataContextState);
 
@@ -19,15 +35,29 @@ type DataProviderProps = { children: ReactNode };
 export const DataProvider = ({ children }: DataProviderProps) => {
   const [dataLoaderStatus, setDataLoaderStatus] = useState<boolean>(false);
   const [customSelectStatus, setCustomSelectStatus] = useState<boolean>(false);
+  const [addBlogFormStatus, setAddBlogFormStatus] = useState<boolean>(false);
+  const [addBlogTitleInputValue, setAddBlogTitleInputValue] =
+    useState<string>('');
+  const [addBlogKeyWordInputValue, setAddBlogKeyWordInputValue] =
+    useState<string>('');
+  const [addBlogContentInputValue, setAddBlogContentInputValue] =
+    useState<string>('');
 
   return (
     <DataContext.Provider
       value={{
         dataLoaderStatus,
         customSelectStatus,
-
+        addBlogFormStatus,
+        addBlogTitleInputValue,
+        addBlogKeyWordInputValue,
+        addBlogContentInputValue,
         setDataLoaderStatus,
         setCustomSelectStatus,
+        setAddBlogFormStatus,
+        setAddBlogTitleInputValue,
+        setAddBlogKeyWordInputValue,
+        setAddBlogContentInputValue,
       }}
     >
       {children}
