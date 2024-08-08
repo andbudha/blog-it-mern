9646b454ecf48cfd150be0c0cad554efb6734e79
andbudha <andreibartov@gmail.com>
2024-08-08 12:24:25 +0200

    BlogCard updated/restyled

--- client/src/components/BlogCard/BlogCard.tsx
+++ client/src/components/BlogCard/BlogCard.tsx
@@ -1,5 +1,5 @@
 import styles from './BlogCard.module.scss';
-import { FiThumbsUp } from 'react-icons/fi';
+import { FaRegHeart } from 'react-icons/fa';
 import { AiOutlineRead } from 'react-icons/ai';
 import { NavLink } from 'react-router-dom';
 import { BlogResponse } from '../../types/common_types';
@@ -8,23 +8,32 @@ type BlogCardProps = {
   blog: BlogResponse;
 };
 export const BlogCard = ({ blog }: BlogCardProps) => {
+  const splitBlogCardContent = blog.content.slice(0, 220);
   return (
     <NavLink className={styles.main_blog_card_box} to={'/blog'}>
       <div className={styles.blog_card_image_box}>
         <img className={styles.blog_image} src={blog.image} />
       </div>
       <div className={styles.blog_card_details_box}>
-        <h4 className={styles.blog_card_title}>{blog.title}</h4>
-        <h5 className={styles.blog_card_author}>
-          by: {blog.user.firstName} {blog.user.lastName}
-        </h5>
-        <pre className={styles.blog_card_text_body}>{blog.content}</pre>
+        <div className={styles.blog_card_title_box}>
+          <h4 className={styles.blog_card_title}>{blog.title}</h4>
+        </div>
+        <div className={styles.blog_card_author_box}>
+          <h5 className={styles.blog_card_author}>
+            by: {blog.user.firstName} {blog.user.lastName}
+          </h5>
+        </div>
+        <div className={styles.blog_card_text_body_box}>
+          <p className={styles.blog_card_text_body}>
+            {splitBlogCardContent}...
+          </p>
+        </div>
       </div>
       <div className={styles.blog_card_bottom_box}>
         {' '}
         <div className={styles.likes_box}>
-          <FiThumbsUp className={styles.like_icon} />
-          <div className={styles.likes_amount}>35</div>
+          <FaRegHeart className={styles.like_icon} />
+          <div className={styles.likes_amount}>{blog.likes.length}</div>
         </div>
         <div className={styles.read_more_box}>
           <AiOutlineRead className={styles.book_icon} />
