81654d4494d18e56352a27b5b5c48cccc04d4496
andbudha <andreibartov@gmail.com>
2024-08-09 11:05:58 +0200

    DetailedBlog page responsiveness implemented

--- client/src/pages/DetailedBlog/DetailedBlog.tsx
+++ client/src/pages/DetailedBlog/DetailedBlog.tsx
@@ -3,6 +3,7 @@ import styles from './DetailedBlog.module.scss';
 import { useContext } from 'react';
 import { DataContext } from '../../contexts/DataContext';
 import { PiUserLight } from 'react-icons/pi';
+import { RiThumbUpLine, RiThumbUpFill } from 'react-icons/ri';
 import { blogAlternativeImage } from '../../assets/utils/blogAlternativeImage';
 
 export const DetailedBlog = () => {
@@ -11,7 +12,7 @@ export const DetailedBlog = () => {
   const blog = blogs?.find((blog) => blog._id === blogID);
   const date = new Date(blog?.createdAt!).toLocaleDateString();
   const time = new Date(blog?.createdAt!).toLocaleTimeString();
-  console.log(blog);
+  const liked = 0;
 
   return (
     <div className={styles.main_detailed_blog_box}>
@@ -31,28 +32,40 @@ export const DetailedBlog = () => {
           <p>{blog?.content}</p>
         </div>
         <div className={styles.blog_footer_box}>
-          <div className={styles.details_box}>
-            <div className={styles.user_full_name_box}>
-              <p>
-                by {blog?.user.firstName} {blog?.user.lastName}
-              </p>
+          <div className={styles.footer_likes_box}>
+            <div className={styles.thumb_up_icon_box}>
+              {liked ? (
+                <RiThumbUpFill className={styles.thumb_up_icon} />
+              ) : (
+                <RiThumbUpLine className={styles.thumb_up_icon} />
+              )}
             </div>
-            <div className={styles.blog_date_box}>
-              <p>
-                {time}, {date}
-              </p>
-            </div>
+            <div className={styles.likes_amount_box}>{blog?.likes.length}</div>
           </div>
-          <div className={styles.user_profile_image_box}>
-            {blog?.user.profileImage ? (
-              <img
-                className={styles.user_profile_image}
-                src={blog?.user.profileImage}
-                alt=""
-              />
-            ) : (
-              <PiUserLight className={styles.user_icon} />
-            )}
+          <div className={styles.footer_info_box}>
+            <div className={styles.details_box}>
+              <div className={styles.user_full_name_box}>
+                <p>
+                  by {blog?.user.firstName} {blog?.user.lastName}
+                </p>
+              </div>
+              <div className={styles.blog_date_box}>
+                <p>
+                  {time}, {date}
+                </p>
+              </div>
+            </div>
+            <div className={styles.user_profile_image_box}>
+              {blog?.user.profileImage ? (
+                <img
+                  className={styles.user_profile_image}
+                  src={blog?.user.profileImage}
+                  alt=""
+                />
+              ) : (
+                <PiUserLight className={styles.user_icon} />
+              )}
+            </div>
           </div>
         </div>
       </div>
