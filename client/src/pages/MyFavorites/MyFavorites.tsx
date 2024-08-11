eafe22b8f760b13544cf7939432e70afc51e4009
andbudha <andreibartov@gmail.com>
2024-08-11 17:41:24 +0200

    info text and home link btn displayed if fav.-blogs-list is empty

--- client/src/pages/MyFavorites/MyFavorites.tsx
+++ client/src/pages/MyFavorites/MyFavorites.tsx
@@ -3,10 +3,11 @@ import styles from './MyFavorites.module.scss';
 import { AuthContext } from '../../contexts/AuthContext';
 import { DataContext } from '../../contexts/DataContext';
 import { BlogCard } from '../../components/BlogCard/BlogCard';
+import { NavLink } from 'react-router-dom';
 
 export const MyFavorites = () => {
   const { user } = useContext(AuthContext);
-  const { favoriteBlogs, fetchFavorites } = useContext(DataContext);
+  const { blogs, favoriteBlogs, fetchFavorites } = useContext(DataContext);
 
   const favoriteBlogsGrid = favoriteBlogs?.map((blog) => (
     <BlogCard key={blog._id} blog={blog} />
@@ -14,5 +15,19 @@ export const MyFavorites = () => {
   useEffect(() => {
     fetchFavorites(user!.userID);
   }, []);
-  return <div className={styles.main_myfavorites_box}>{favoriteBlogsGrid}</div>;
+
+  return (
+    <div className={styles.main_myfavorites_box}>
+      {!favoriteBlogs?.length ? (
+        <div className={styles.info_box}>
+          <h2 className={styles.info_text}>No Favorite Blogs Added</h2>
+          <NavLink className={styles.home_button} to={'/'}>
+            main
+          </NavLink>
+        </div>
+      ) : (
+        favoriteBlogsGrid
+      )}
+    </div>
+  );
 };
