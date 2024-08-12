c6f03b7f5d43d9530fef63be6f14afd4bf4de163
andbudha <andreibartov@gmail.com>
2024-08-12 14:48:05 +0200

    DetailedBlog comp updated

--- client/src/pages/DetailedBlog/DetailedBlog.tsx
+++ client/src/pages/DetailedBlog/DetailedBlog.tsx
@@ -11,16 +11,19 @@ import { blogAlternativeImage } from '../../assets/utils/blogAlternativeImage';
 import { AuthContext } from '../../contexts/AuthContext';
 import { notificationToast } from '../../assets/toasts/notificationToast';
 import { AuthLoader } from '../../components/Loaders/AuthLoader/AuthLoader';
+import { BlogEditForm } from '../../components/Forms/BlogEditForm/BlogEditForm';
 
 export const DetailedBlog = () => {
   const { blogID } = useParams();
   const { user, authLoaderStatus } = useContext(AuthContext);
   const {
+    displayBlogEditFormStatus,
     displayPopupWindowStatus,
     informStatus,
     blogs,
     toggleBlogLiking,
     deleteBlog,
+    setDisplayBlogEditFormStatus,
     setDisplayPopupWindowStatus,
   } = useContext(DataContext);
   const [displayTextareaStatus, setDisplayTextAreaStatus] =
@@ -55,13 +58,16 @@ export const DetailedBlog = () => {
     deleteBlog(blogID);
   };
 
+  const displayBlogEditFormHandler = () => {
+    setDisplayBlogEditFormStatus(!displayBlogEditFormStatus);
+  };
   if (informStatus) {
     return <Navigate to={'/info-page'} />;
   }
   return (
     <div className={styles.main_detailed_blog_box}>
       {displayPopupWindowStatus && (
-        <div className={styles.popup_box}>
+        <div className={styles.main_popup_box}>
           <div className={styles.popup_window}>
             {authLoaderStatus === 'deleting' && <AuthLoader />}
             <div className={styles.info_text_box}>
@@ -84,81 +90,90 @@ export const DetailedBlog = () => {
           </div>
         </div>
       )}
+      {displayBlogEditFormStatus ? (
+        <BlogEditForm blog={blog} />
+      ) : (
+        <div className={styles.detailed_blog_box}>
+          <div className={styles.blog_image_box}>
+            <img
+              className={styles.blog_image}
+              src={blog?.image ? blog.image : blogAlternativeImage}
+              alt="blog cover image"
+            />
+          </div>
 
-      <div className={styles.detailed_blog_box}>
-        <div className={styles.blog_image_box}>
-          <img
-            className={styles.blog_image}
-            src={blog?.image ? blog.image : blogAlternativeImage}
-            alt="blog cover image"
-          />
+          <div className={styles.blog_title_box}>
+            <h2>{blog?.title}</h2>
+          </div>
+          <div className={styles.blog_content_box}>
+            <p>{blog?.content}</p>
+          </div>
+          <div className={styles.blog_footer_box}>
+            <div className={styles.footer_likes_box}>
+              <div className={styles.thumb_up_icon_box}>
+                {liked?.length === 1 ? (
+                  <RiThumbUpFill
+                    className={styles.thumb_up_icon}
+                    onClick={toggleLikeBlogHandler}
+                  />
+                ) : (
+                  <RiThumbUpLine
+                    className={styles.thumb_up_icon}
+                    onClick={toggleLikeBlogHandler}
+                  />
+                )}
+              </div>
+              <div className={styles.likes_amount_box}>
+                {blog?.likes.length}
+              </div>
+            </div>
+            <div className={styles.footer_info_box}>
+              <div className={styles.details_box}>
+                <div className={styles.user_full_name_box}>
+                  <p>
+                    by {blog?.user.firstName} {blog?.user.lastName}
+                  </p>
+                </div>
+                <div className={styles.blog_date_box}>
+                  <p>
+                    {time}, {date}
+                  </p>
+                </div>
+              </div>
+              <div className={styles.user_profile_image_box}>
+                {blog?.user.profileImage ? (
+                  <img
+                    className={styles.user_profile_image}
+                    src={blog?.user.profileImage}
+                    alt=""
+                  />
+                ) : (
+                  <PiUserLight className={styles.user_icon} />
+                )}
+              </div>
+            </div>
+          </div>
+          {user?.userID === blog?.user._id && (
+            <div className={styles.manage_blog_box}>
+              <div className={styles.manage_blog_button_box}>
+                <div
+                  className={styles.edit_blog_button}
+                  onClick={displayBlogEditFormHandler}
+                >
+                  <FiEdit3 className={styles.edit_blog_icon} />
+                </div>
+                <div
+                  className={styles.remove_blog_button}
+                  onClick={displayPopupWindowHandler}
+                >
+                  <MdOutlineDelete className={styles.remove_blog_icon} />
+                </div>
+              </div>
+            </div>
+          )}
         </div>
+      )}
 
-        <div className={styles.blog_title_box}>
-          <h2>{blog?.title}</h2>
-        </div>
-        <div className={styles.blog_content_box}>
-          <p>{blog?.content}</p>
-        </div>
-        <div className={styles.blog_footer_box}>
-          <div className={styles.footer_likes_box}>
-            <div className={styles.thumb_up_icon_box}>
-              {liked?.length === 1 ? (
-                <RiThumbUpFill
-                  className={styles.thumb_up_icon}
-                  onClick={toggleLikeBlogHandler}
-                />
-              ) : (
-                <RiThumbUpLine
-                  className={styles.thumb_up_icon}
-                  onClick={toggleLikeBlogHandler}
-                />
-              )}
-            </div>
-            <div className={styles.likes_amount_box}>{blog?.likes.length}</div>
-          </div>
-          <div className={styles.footer_info_box}>
-            <div className={styles.details_box}>
-              <div className={styles.user_full_name_box}>
-                <p>
-                  by {blog?.user.firstName} {blog?.user.lastName}
-                </p>
-              </div>
-              <div className={styles.blog_date_box}>
-                <p>
-                  {time}, {date}
-                </p>
-              </div>
-            </div>
-            <div className={styles.user_profile_image_box}>
-              {blog?.user.profileImage ? (
-                <img
-                  className={styles.user_profile_image}
-                  src={blog?.user.profileImage}
-                  alt=""
-                />
-              ) : (
-                <PiUserLight className={styles.user_icon} />
-              )}
-            </div>
-          </div>
-        </div>
-        {user?.userID === blog?.user._id && (
-          <div className={styles.manage_blog_box}>
-            <div className={styles.manage_blog_button_box}>
-              <div className={styles.edit_blog_button}>
-                <FiEdit3 className={styles.edit_blog_icon} />
-              </div>
-              <div
-                className={styles.remove_blog_button}
-                onClick={displayPopupWindowHandler}
-              >
-                <MdOutlineDelete className={styles.remove_blog_icon} />
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
       <div className={styles.comment_text_area_box}>
         <div
           className={styles.display_textarea_button}
