bbe06d17c932363b16227d46ca5424c8eb8cbd66
andbudha <andreibartov@gmail.com>
2024-08-13 14:16:28 +0200

    Commentary comp updated/restyled/props received

--- client/src/components/Commentary/Commentray.tsx
+++ client/src/components/Commentary/Commentray.tsx
@@ -1,12 +1,17 @@
 import { FiEdit3 } from 'react-icons/fi';
-import { BlogResponse } from '../../types/common_types';
+import { CommentaryValues } from '../../types/common_types';
 import styles from './Commentray.module.scss';
 import { MdOutlineDelete } from 'react-icons/md';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 type CommentaryProps = {
-  blog: BlogResponse | undefined;
+  commentary: CommentaryValues;
 };
-export const Commentary = ({ blog }: CommentaryProps) => {
+export const Commentary = ({ commentary }: CommentaryProps) => {
+  const { user } = useContext(AuthContext);
+  const date = new Date(commentary.createdAt!).toLocaleDateString();
+  const time = new Date(commentary.createdAt!).toLocaleTimeString();
   return (
     <div className={styles.main_commentary_box}>
       <div className={styles.secondary_commentary_author_box}>
@@ -14,34 +19,33 @@ export const Commentary = ({ blog }: CommentaryProps) => {
           <div className={styles.author_image_box}>
             <img
               className={styles.author_image}
-              src={blog?.user.profileImage}
+              src={commentary.profileImage}
               alt="author image"
             />
           </div>
           <div className={styles.author_full_name_box}>
-            <p>{blog?.user.firstName}</p>
-            <p>{blog?.user.lastName}</p>
+            <p>{commentary.firstName}</p>
+            <p>{commentary.lastName}</p>
           </div>
         </div>
-        <div className={styles.commentary_manage_box}>
-          <button className={styles.edit_commentary_button_box}>
-            {' '}
-            <FiEdit3 className={styles.edit_icon} />
-          </button>
-          <button className={styles.remove_commentary_button_box}>
-            {' '}
-            <MdOutlineDelete className={styles.remove_icon} />
-          </button>
-        </div>
+        {commentary.userID === user?.userID && (
+          <div className={styles.commentary_manage_box}>
+            <button className={styles.edit_commentary_button_box}>
+              <FiEdit3 className={styles.edit_icon} />
+            </button>
+            <button className={styles.remove_commentary_button_box}>
+              <MdOutlineDelete className={styles.remove_icon} />
+            </button>
+          </div>
+        )}
       </div>
       <div className={styles.commentary_body_box}>
-        <p className={styles.commentary_body}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla saepe
-          neque eius non corporis sunt eveniet accusantium sequi quam veritatis!
-        </p>
+        <p className={styles.commentary_body}>{commentary.commentary}</p>
       </div>
       <div className={styles.commentary_footer_box}>
-        <p className={styles.commentary_date}>4:07:26 PM, 8/12/2024</p>
+        <p className={styles.commentary_date}>
+          {time}, {date}
+        </p>
       </div>
     </div>
   );
