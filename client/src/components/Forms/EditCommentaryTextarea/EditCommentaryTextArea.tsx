86ed8bb3a1deca7b50826ee8c42d6b8ba3ef2528
andbudha <andreibartov@gmail.com>
2024-08-14 15:41:59 +0200

    EditCommentaryTextArea comp removed

--- client/src/components/Forms/EditCommentaryTextarea/EditCommentaryTextArea.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './EditCommentaryTextarea.module.scss';
-
-export const EditCommentaryTextarea = () => {
-  return (
-    <div className={styles.main_textarea_box}>
-      <textarea className={styles.textarea_box} />
-    </div>
-  );
-};
