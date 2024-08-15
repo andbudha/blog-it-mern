01a72b72cbea0b266055bee5f59fa5e5a6623fe9
andbudha <andreibartov@gmail.com>
2024-08-15 10:34:46 +0200

    comment editing and loader displaying func.-ties implemented

--- client/src/components/Commentary/Commentray.tsx
+++ client/src/components/Commentary/Commentray.tsx
@@ -2,11 +2,10 @@ import { FiEdit3, FiX, FiCheck } from 'react-icons/fi';
 import { CommentaryValues } from '../../types/common_types';
 import styles from './Commentray.module.scss';
 import { MdOutlineDelete } from 'react-icons/md';
-import { useContext, useState } from 'react';
+import { ChangeEvent, useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import { DataContext } from '../../contexts/DataContext';
 import { AuthLoader } from '../Loaders/AuthLoader/AuthLoader';
-import { EditCommentaryTextarea } from '../Forms/EditCommentaryTextarea/EditCommentaryTextArea';
 import { notificationToast } from '../../assets/toasts/notificationToast';
 
 type CommentaryProps = {
@@ -14,29 +13,51 @@ type CommentaryProps = {
 };
 export const Commentary = ({ commentary }: CommentaryProps) => {
   const { user, authLoaderStatus } = useContext(AuthContext);
-  const { deleteCommentary } = useContext(DataContext);
+  const { deleteCommentary, editCommentary } = useContext(DataContext);
   const [showEditTextarea, setShowEditTextarea] = useState<boolean>(false);
+  const [editCommentaryTextareaValue, setEditCommentaryTextareaValue] =
+    useState<string>(commentary.commentary);
   const date = new Date(commentary.createdAt!).toLocaleDateString();
   const time = new Date(commentary.createdAt!).toLocaleTimeString();
-  console.log(showEditTextarea);
 
-  const changeEditTextareaStatusHandler = () => {
-    setShowEditTextarea(true);
+  const changeEditTextareaStatusHandler = (commentaryID: string) => {
+    setShowEditTextarea(!showEditTextarea);
   };
   const deleteCommentaryHandler = (blogID: string, commentaryID: string) => {
     deleteCommentary(blogID, commentaryID);
   };
-
-  const saveCommentaryChangesHandler = () => {
-    notificationToast('Changes saved!');
+  const catchingEditCommentaryTextareaHandler = (
+    e: ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setEditCommentaryTextareaValue(e.currentTarget.value);
+  };
+  const saveCommentaryChangesHandler = (
+    blogID: string,
+    commentaryID: string
+  ) => {
+    const editedCommentaryBody = {
+      blogID,
+      commentaryID,
+      newCommentary: editCommentaryTextareaValue.trim(),
+    };
+    if (editCommentaryTextareaValue.trim().length === 0) {
+      notificationToast('Write a commentary first before posting!');
+    } else {
+      editCommentary(editedCommentaryBody);
+      setShowEditTextarea(!showEditTextarea);
+    }
   };
   const discardCommentaryChangesHandler = () => {
-    setShowEditTextarea(false);
+    setShowEditTextarea(!showEditTextarea);
+    setEditCommentaryTextareaValue(commentary.commentary);
   };
   return (
     <div className={styles.main_commentary_box}>
-      {user?.userID === commentary.userID &&
-        authLoaderStatus === 'deleting' && <AuthLoader />}
+      {(user?.userID === commentary.userID &&
+        authLoaderStatus === 'deleting' && <AuthLoader />) ||
+        (user?.userID === commentary.userID &&
+          authLoaderStatus === 'editing' && <AuthLoader />)}
+
       <div className={styles.secondary_commentary_author_box}>
         <div className={styles.commentary_author_box}>
           <div className={styles.author_image_box}>
@@ -56,16 +77,21 @@ export const Commentary = ({ commentary }: CommentaryProps) => {
             {showEditTextarea ? (
               <button
                 className={styles.save_commentary_changes_button}
-                onClick={saveCommentaryChangesHandler}
+                onClick={() =>
+                  saveCommentaryChangesHandler(
+                    commentary.blogID,
+                    commentary._id!
+                  )
+                }
               >
                 <FiCheck className={styles.save_changes_icon} />
               </button>
             ) : (
-              <button className={styles.edit_commentary_button}>
-                <FiEdit3
-                  className={styles.edit_icon}
-                  onClick={changeEditTextareaStatusHandler}
-                />
+              <button
+                className={styles.edit_commentary_button}
+                onClick={() => changeEditTextareaStatusHandler(commentary._id!)}
+              >
+                <FiEdit3 className={styles.edit_icon} />
               </button>
             )}
             {showEditTextarea ? (
@@ -88,13 +114,24 @@ export const Commentary = ({ commentary }: CommentaryProps) => {
           </div>
         )}
       </div>
-      {showEditTextarea ? (
-        <EditCommentaryTextarea commentary={commentary} />
-      ) : (
-        <div className={styles.commentary_body_box}>
-          <p className={styles.commentary_body}>{commentary.commentary}</p>
-        </div>
-      )}
+      <div className={styles.commentary_body_box}>
+        {showEditTextarea ? (
+          <div className={styles.main_textarea_box}>
+            <textarea
+              value={editCommentaryTextareaValue}
+              className={styles.textarea_box}
+              onChange={catchingEditCommentaryTextareaHandler}
+            />
+          </div>
+        ) : (
+          <div className={styles.commentary_box}>
+            {' '}
+            <pre className={styles.commentary}>
+              {editCommentaryTextareaValue || commentary.commentary}
+            </pre>
+          </div>
+        )}
+      </div>
       <div className={styles.commentary_footer_box}>
         <p className={styles.commentary_date}>
           {time}, {date}
