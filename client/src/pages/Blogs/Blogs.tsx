c9e081f20450312ded2d6e15ca283caf5043bf49
andbudha <andreibartov@gmail.com>
2024-08-16 13:02:30 +0200

    Blogs comp restyled/Paginator comp injected

--- client/src/pages/Blogs/Blogs.tsx
+++ client/src/pages/Blogs/Blogs.tsx
@@ -1,13 +1,26 @@
 import { useContext } from 'react';
 import { BlogCard } from '../../components/BlogCard/BlogCard';
 import styles from './Blogs.module.scss';
-import { DataContext } from '../../contexts/DataContext';
+import { PaginationContext } from '../../contexts/PaginationContext';
+import { Paginator } from '../../components/Paginator/Paginator';
+import { AuthContext } from '../../contexts/AuthContext';
+import { AuthLoader } from '../../components/Loaders/AuthLoader/AuthLoader';
 
 export const Blogs = () => {
-  const { blogs } = useContext(DataContext);
+  const { blogsToDisplayPerPage } = useContext(PaginationContext);
+  const { authLoaderStatus } = useContext(AuthContext);
 
-  const blogGrid = blogs?.map((blog) => (
+  const blogGrid = blogsToDisplayPerPage?.map((blog) => (
     <BlogCard key={blog._id} blog={blog} />
   ));
-  return <div className={styles.main_blogs_box}>{blogGrid}</div>;
+  return (
+    <div className={styles.main_blogs_box}>
+      <div className={styles.blogs_box}>
+        {!blogGrid?.length ? <AuthLoader /> : blogGrid}
+      </div>
+      <div className={styles.paginator_box}>
+        <Paginator />
+      </div>
+    </div>
+  );
 };
