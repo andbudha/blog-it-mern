d7dd856a1c3dcc1114d4a9cda0604cb60bd96f72
andbudha <andreibartov@gmail.com>
2024-08-16 12:59:57 +0200

    PaginationContext updated

--- client/src/contexts/PaginationContext.tsx
+++ client/src/contexts/PaginationContext.tsx
@@ -1,15 +1,58 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { DataContext } from './DataContext';
+import { BlogResponse } from '../types/common_types';
 
-type PaginationContextType = {};
-const initialPaginationContextState = {} as PaginationContextType;
-export const PaginationContext = createContext(initialPaginationContextState);
-
+type PaginationContextType = {
+  currentPage: number;
+  numberOfPages: number;
+  setCurrentPage: (pageNumber: number) => void;
+  blogsToDisplayPerPage: undefined | BlogResponse[];
+};
 type PaginationProviderProps = { children: ReactNode };
 
+const initialPaginationContextState = {
+  currentPage: 0,
+  numberOfPages: 0,
+  setCurrentPage: (pageNumber: number) => pageNumber,
+  blogsToDisplayPerPage: [] as BlogResponse[],
+} as PaginationContextType;
+
+export const PaginationContext = createContext(initialPaginationContextState);
+
 export const PaginationProvider = ({ children }: PaginationProviderProps) => {
+  const { blogs } = useContext(DataContext);
+
+  const blogsPerPage = 6;
+  const numberOfPages = Math.ceil(blogs ? blogs.length / blogsPerPage : 0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const start = blogsPerPage * (currentPage - 1);
+  const end = start + blogsPerPage;
+  const blogsToDisplayPerPage = blogs?.slice(start, end);
+
   return (
-    <PaginationContext.Provider value={{}}>
+    <PaginationContext.Provider
+      value={{
+        currentPage,
+        numberOfPages,
+        setCurrentPage,
+        blogsToDisplayPerPage,
+      }}
+    >
       {children}
     </PaginationContext.Provider>
   );
 };
+
+// type PaginationContextType = {};
+// const initialPaginationContextState = {} as PaginationContextType;
+// export const PaginationContext = createContext(initialPaginationContextState);
+
+// type PaginationProviderProps = { children: ReactNode };
+
+// export const PaginationProvider = ({ children }: PaginationProviderProps) => {
+//   return (
+//     <PaginationContext.Provider value={{}}>
+//       {children}
+//     </PaginationContext.Provider>
+//   );
+// };
