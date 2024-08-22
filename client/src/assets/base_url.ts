e11dc81df2d4f6a33872f6a4e2dcdbc43576467e
andbudha <andreibartov@gmail.com>
2024-08-22 13:24:08 +0200

    both development & preview app mode for baseURL added

--- client/src/assets/base_url.ts
+++ client/src/assets/base_url.ts
@@ -1,2 +1,7 @@
 // export const baseUrl = 'http://localhost:5000/blogit';
-export const baseUrl = 'https://blog-it-mern-server.vercel.app/';
+// export const baseUrl = 'https://blog-it-mern-server.vercel.app';
+
+export const baseUrl =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5000/blogit'
+    : 'https://blog-it-mern-server.vercel.app';
