39b213c339a9b3f2ac2bb8b895541f076d10e1fa
andbudha <andreibartov@gmail.com>
2024-08-23 11:34:46 +0200

    base url updated

--- client/src/assets/base_url.ts
+++ client/src/assets/base_url.ts
@@ -4,4 +4,4 @@
 export const baseUrl =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5000/blogit'
-    : 'https://blog-it-mern-server.vercel.app';
+    : 'https://blog-it-mern-server.vercel.app/blogit';
